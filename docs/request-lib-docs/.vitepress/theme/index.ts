98fe6963d1773a16476cca32f0df67bfec6178c2
nextc <1391040917@qq.com>
2025-09-16 13:47:36 +0800

    docs: 渲染 mermaid

--- docs/request-lib-docs/.vitepress/theme/index.ts
+++ docs/request-lib-docs/.vitepress/theme/index.ts
@@ -1,11 +1,24 @@
+import { nextTick } from 'vue'
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+
 import { NaiveUIContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
-import type { App } from 'vue'
+
+import { createMermaidRenderer } from 'vitepress-mermaid-renderer'
+import 'vitepress-mermaid-renderer/dist/style.css'
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }: { app: App }) {
+  enhanceApp({ app, router }) {
     app.component('demo-preview', NaiveUIContainer)
+
+    const mermaidRenderer = createMermaidRenderer()
+    mermaidRenderer.initialize()
+    if (router) {
+      router.onAfterRouteChange = () => {
+        nextTick(() => mermaidRenderer.renderMermaidDiagrams())
+      }
+    }
   },
-}
+} satisfies Theme
