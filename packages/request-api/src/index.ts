10c0c32e96328abdb0e2cc775e60fe82ca2836f8
nextc <1391040917@qq.com>
2025-10-13 10:33:51 +0800

    feat: 移除不必要的 createRequestCore 导出

--- packages/request-api/src/index.ts
+++ packages/request-api/src/index.ts
@@ -1,9 +1,6 @@
 // 导出类型和接口
 export type { ApiClass, ApiInstance } from './types'
 
-// 导出核心工厂方法
-export { createRequestCore } from './core/factory'
-
 // 导出客户端相关
 export type { ApiClient, ApiClientOptions } from './client/api-client'
 export { createApiClient } from './client/api-client'
